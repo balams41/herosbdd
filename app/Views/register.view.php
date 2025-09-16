e337685c153ff94f2e75000583943b83698370e2
balams41 <balamslad41@gmail.com>
2025-09-16 14:23:28 +0200

    recuperer le formulaire

--- app/Views/register.view.php
+++ app/Views/register.view.php
@@ -1,16 +1,30 @@
 <?php 
 require_once(__DIR__ . '/partials/head.php');
 ?>
-h1>Inscription</h1>
+<h1>Inscription</h1>
 <form method="POST">
     <div class="container">
         <div class="form-group">
             <label for="pseudo" class="form-label">Donne moi ton pseudo :</label>
             <input type="text" name="pseudo" id="pseudo" placeholder="Toto" class="form-control">
+            <?php 
+            if(isset($arrayError['pseudo'])){
+                ?>
+                    <p class="text-danger"><?= $arrayError['pseudo']?></p>
+                <?php
+            }
+            ?>
         </div>
         <div class="form-group">
             <label for="email" class="form-label">Donne moi ton email :</label>
             <input type="email" name="email" id="email" placeholder="Toto@gmail.com" class="form-control">
+               <?php 
+            if(isset($arrayError['email'])){
+                ?>
+                    <p class="text-danger"><?= $arrayError['email']?></p>
+                <?php
+            }
+            ?>
         </div>
         <div class="form-group">
             <label for="password" class="form-label">Donne moi ton mot de pase :</label>
@@ -19,6 +33,7 @@ h1>Inscription</h1>
         <button type="submit" class="btn btn-success mt-5">Inscription</button>
     </div>
 </form>
+
 <?php 
 require_once(__DIR__ . '/partials/footer.php');
 ?>
